92f3329d5eae64227d3e401a6705e91a8b4d2b2f
Wak017 <cinnamon017@hotmail.com>
2016-12-01 17:48:52 -0500

    Update human.js

--- client/human.js
+++ client/human.js
@@ -3,31 +3,8 @@ var me = 0;
 var cells = [];
 var player1;
 var player2;
-
-var createMatrix = function(width, height){
-
-  for(var i = 0; i < width; i++) {
-
-    cells[i] = Array(height);
-  }
-
-  fill(cells, " ");
-  return cells;
-}
-
-var afficherObstacles = function(x, y, width, height, limiteW, limiteH){
-
-  for(var k = 0; k < width; k++){
-
-    for(var l = 0; l < height; l++){
-
-      if(x+k >= 0 && y+l >= 0 && x+k < limiteH && y+l < limiteW){
-        cells[x+k][y+l] = '#';
-        Grid.colorCell(x+k, y+l, 'green');
-      }
-    }
-  } return cells;
-};
+var cellsWidth;
+var cellsHeight;
 
 var createGrid = function(config) {
     /**
@@ -98,6 +75,85 @@ var victory = function(winner) {
      * TODO : mettre à jour la grille en indiquant d'une couleur différente
      * le/les joueurs morts.
      */
+     var m = Math.floor(cellsWidth/2);
+     var n = Math.floor(cellsHeight/2);
+
+     console.log(winner);
+
+     var gagnant = function(){
+
+       Grid.colorCell(m-13, n-2, 'white'); Grid.colorCell(m-13, n-1, 'white'); Grid.colorCell(m-13, n, 'white');
+       Grid.colorCell(m-13, n+1, 'white'); Grid.colorCell(m-13, n+2, 'white'); Grid.colorCell(m-12, n-2, 'white');
+       Grid.colorCell(m-12, n, 'white'); Grid.colorCell(m-11, n-2, 'white'); Grid.colorCell(m-11, n-1, 'white');
+       Grid.colorCell(m-11, n, 'white'); Grid.colorCell(m-4, n-2, 'white'); Grid.colorCell(m-4, n-1, 'white');
+       Grid.colorCell(m-4, n, 'white'); Grid.colorCell(m-4, n+1, 'white'); Grid.colorCell(m-3, n+2, 'white');
+       Grid.colorCell(m-2, n-1, 'white'); Grid.colorCell(m-2, n, 'white'); Grid.colorCell(m-2, n+1, 'white');
+       Grid.colorCell(m-1, n+2, 'white'); Grid.colorCell(m, n-2, 'white'); Grid.colorCell(m, n-1, 'white');
+       Grid.colorCell(m, n, 'white'); Grid.colorCell(m, n+1, 'white'); Grid.colorCell(m+2, n-2, 'white');
+       Grid.colorCell(m+2, n+2, 'white'); Grid.colorCell(m+3, n-2, 'white'); Grid.colorCell(m+3, n-1, 'white');
+       Grid.colorCell(m+3, n, 'white'); Grid.colorCell(m+3, n+1, 'white'); Grid.colorCell(m+3, n+2, 'white');
+       Grid.colorCell(m+4, n-2, 'white'); Grid.colorCell(m+4, n+2, 'white'); Grid.colorCell(m+6, n-2, 'white');
+       Grid.colorCell(m+6, n-1, 'white'); Grid.colorCell(m+6, n, 'white'); Grid.colorCell(m+6, n+1, 'white');
+       Grid.colorCell(m+6, n+2, 'white'); Grid.colorCell(m+7, n, 'white'); Grid.colorCell(m+8, n+1, 'white');
+       Grid.colorCell(m+9, n-2, 'white'); Grid.colorCell(m+9, n-1, 'white'); Grid.colorCell(m+9, n, 'white');
+       Grid.colorCell(m+9, n+1, 'white'); Grid.colorCell(m+9, n+2, 'white'); Grid.colorCell(m+11, n-1, 'white');
+       Grid.colorCell(m+11, n+2, 'white'); Grid.colorCell(m+12, n-2, 'white'); Grid.colorCell(m+12, n, 'white');
+       Grid.colorCell(m+12, n+2, 'white'); Grid.colorCell(m+13, n-2, 'white'); Grid.colorCell(m+13, n+1, 'white');
+
+     };
+
+     var perdant = function(joueur){
+
+
+       for(var y = 0; y < cellsWidth; y++){
+
+         for(var z = 0; z < cellsHeight; z++){
+
+           if(cells[y][z] == joueur){
+
+             Grid.colorCell(y, z, 'gray');
+           }
+         }
+       }
+     }
+
+     if(winner == 1){
+
+       Grid.colorCell(m-9, n-1, 'white'); Grid.colorCell(m-9, n+2, 'white'); Grid.colorCell(m-8, n-2, 'white');
+       Grid.colorCell(m-8, n-1, 'white'); Grid.colorCell(m-8, n, 'white'); Grid.colorCell(m-8, n+1, 'white');
+       Grid.colorCell(m-8, n+2, 'white'); Grid.colorCell(m-7, n+2, 'white');
+       Grid.colorCell(player2.x, player2.y, 'gray');
+
+       perdant(winner+1);
+       gagnant();
+     }
+     else if(winner == 2){
+
+       Grid.colorCell(m-9, n-2, 'white'); Grid.colorCell(m-9, n+2, 'white'); Grid.colorCell(m-8, n-2, 'white');
+       Grid.colorCell(m-8, n+1, 'white'); Grid.colorCell(m-8, n+2, 'white'); Grid.colorCell(m-7, n-1, 'white');
+       Grid.colorCell(m-7, n, 'white'); Grid.colorCell(m-7, n+2, 'white');
+       Grid.colorCell(player1.x, player1.y, 'gray');
+
+       perdant(winner-1);
+       gagnant();
+     }
+     else{
+
+       Grid.colorCell(m-8, n-2, 'white'); Grid.colorCell(m-8, n-1, 'white'); Grid.colorCell(m-8, n, 'white');
+       Grid.colorCell(m-8, n+1, 'white'); Grid.colorCell(m-8, n+2, 'white'); Grid.colorCell(m-7, n-2, 'white');
+       Grid.colorCell(m-7, n+2, 'white'); Grid.colorCell(m-6, n-1, 'white'); Grid.colorCell(m-6, n, 'white');
+       Grid.colorCell(m-6, n+1, 'white'); Grid.colorCell(m-4, n-2, 'white'); Grid.colorCell(m-4, n-1, 'white');
+       Grid.colorCell(m-4, n, 'white'); Grid.colorCell(m-4, n+1, 'white'); Grid.colorCell(m-4, n+2, 'white');
+       Grid.colorCell(m-3, n-2, 'white'); Grid.colorCell(m-3, n, 'white'); Grid.colorCell(m-2, n-1, 'white');
+       Grid.colorCell(m-2, n+1, 'white'); Grid.colorCell(m-2, n+2, 'white'); Grid.colorCell(m, n-1, 'white');
+       Grid.colorCell(m, n, 'white'); Grid.colorCell(m, n+1, 'white'); Grid.colorCell(m, n+2, 'white');
+       Grid.colorCell(m+1, n-2, 'white'); Grid.colorCell(m+1, n, 'white'); Grid.colorCell(m+2, n-1, 'white');
+       Grid.colorCell(m+2, n, 'white'); Grid.colorCell(m+2, n+1, 'white'); Grid.colorCell(m+2, n+2, 'white');
+       Grid.colorCell(m+4, n-2, 'white'); Grid.colorCell(m+4, n-1, 'white'); Grid.colorCell(m+4, n, 'white');
+       Grid.colorCell(m+4, n+1, 'white'); Grid.colorCell(m+5, n+2, 'white'); Grid.colorCell(m+6, n+1, 'white');
+       Grid.colorCell(m+7, n+2, 'white'); Grid.colorCell(m+8, n-2, 'white'); Grid.colorCell(m+8, n-1, 'white');
+       Grid.colorCell(m+8, n, 'white'); Grid.colorCell(m+8, n+1, 'white');
+     }
 };
 
 /**
@@ -108,6 +164,7 @@ document.addEventListener('keypress', function(evenement) {
     /* regardez ce que contient la variable d'événement dans la console
      * pour déterminer quelle touche a été appuyée
      */
+
     console.log(evenement);
 
     /**
@@ -115,16 +172,166 @@ document.addEventListener('keypress', function(evenement) {
      * `direction` qui sera retournée la prochaine fois que le serveur
      * demandera quelle direction prendre.
      */
-    direction = 'u';
+
+    switch(evenement.key) {
+
+    case 'w':
+
+	direction = 'u';
+	break;
+
+    case 's':
+
+	direction = 'd';
+	break;
+
+    case 'd':
+
+	direction = 'r';
+	break;
+
+    case 'a':
+
+	direction = 'l';
+	break;
+
+    }
 });
+
+/**
+ * direction peut prendre les valeurs :
+ * 'u' (up    => haut)
+ * 'd' (down  => bas)
+ * 'l' (left  => gauche)
+ * 'r' (right => droite)
+ */
+var direction = 'u';
+
+var nextMove = function(previousMoves) {
+    /**
+     * TODO : mettre à jour la grille de jeu affichée selon les positions
+     * des joueurs indiquées dans l'enregistrement `previousMoves`.
+     */
+    /**
+     * Note : ne modifiez pas cette ligne, ni le contenu de la variable
+     * `direction` depuis cette fonction. Cette variable devrait être
+     * modifiée uniquement par le code plus bas, lorsque l'utilisateur
+     * a appuyé sur une touche du clavier.
+     */
+
+    if(previousMoves.length == 2) {
+	updatePlayer(player1, previousMoves[0]);
+	updatePlayer(player2, previousMoves[1]);
+    }
+
+    return direction;
+};
+
+var createMatrix = function(width, height){
+
+  for(var i = 0; i < width; i++) {
+
+    cells[i] = Array(height);
+  }
+
+  fill(cells, " ");
+  return cells;
+}
+
+var afficherObstacles = function(x, y, width, height, limiteW, limiteH){
+
+  for(var k = 0; k < width; k++){
+
+    for(var l = 0; l < height; l++){
+
+      if(x+k >= 0 && y+l >= 0 && x+k < limiteH && y+l < limiteW){
+        cells[x+k][y+l] = '#';
+        if(k == 0 || l == 0){
+
+          Grid.colorCell(x+k, y+l, 'firebrick');
+        }
+        else{
+
+          Grid.colorCell(x+k, y+l, 'maroon');
+        }
+      }
+    }
+  } return cells;
+};
+
+var updatePlayer = function(player, previousMove) {
+
+
+    var color = "blue";
+    var x = player.x;
+    var y = player.y;
+
+    if(player.id == 2) {
+
+	color = "red";
+
+    }
+
+
+    switch(previousMove.direction) {
+
+    case 'u':
+
+	y--;
+	break;
+
+    case 'd':
+
+	y++;
+	break;
+
+    case 'l':
+
+	x--;
+	break;
+
+    case 'r':
+
+	x++;
+	break;
+
+    }
+
+    player.x = x;
+    player.y = y;
+    cells[player.x][player.y] = "" + player.id;
+    Grid.colorCell(x, y, color);
+
+}
+
+var printMatrix = function(matrix) {
+
+    var text = "";
+
+    for(var y = 0; y < matrix[0].length; y++) {
+
+	for(var x = 0; x < matrix.length; x++) {
+
+	    text += matrix[x][y];
+
+	}
+
+	text += "\n";
+
+    }
+
+    console.log(text);
+
+};
 
 var fill = function(matrix, value) {
 
-  for(var i = 0; i < matrix.length; i++) {
+  for(var i = 0; i < matrix.length; i++) {
 
     for(var j = 0; j < matrix[i].length; j++) {
 
       matrix[i][j] = value;
+      Grid.colorCell(i, j, 'black');
 
     }
 
